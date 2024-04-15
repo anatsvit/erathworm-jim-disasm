8200ba3ffe4ba71b8bfbe50dc6667b1966338a14
Vit <anatsvit@gmail.com>
2024-04-16 00:58:25 +0500

    EJIM-2 Названия ссылок в структуре 1B6910

--- jimcompile.bat
+++ jimcompile.bat
@@ -1,5 +1,5 @@
 set aspath=D:\DCDownloads\SEGA_REVERSE\ASSEMBLER\bin
-%aspath%\asw -L -w -maxerrors 2 jim.s
+%aspath%\asw -L -w -maxerrors 10 jim.s
 %aspath%\p2bin jim.p
 echo "Original ROM size: 3145728 bytes"
 DEL jim.p
